234977f5a9950823c55856dbdad6fef9d805b827
nirzaa <72485675+nirzaa@users.noreply.github.com>
2022-02-04 19:07:07 +0200

    updated

--- analyzer.py
+++ analyzer.py
@@ -29,10 +29,13 @@ def weights_anal():
         my_path_weights = os.path.join('./', 'csv_files', f'epoch_{epoch_num}')
         if not os.path.exists(my_path_weights):
             os.makedirs(my_path_weights)
-        my_path = os.path.join('./', 'saved', 'models', 'my_model', '0126_110510', f'checkpoint-epoch{epoch_num}.pth')
+        my_path = os.path.join('./', 'saved', 'models', 'my_model', '0202_223508', f'checkpoint-epoch{epoch_num}.pth')
         checkpoint = torch.load(my_path)
         weights = checkpoint['state_dict']
         layers = list(weights.keys())
+        layers_weights = list(weights.values())
+        for i, layer in enumerate(layers):
+            print(f'{i}. layer: {layer}, weights: {layers_weights[i].shape}')
         for i in [78, 77, 66]:
             ln = weights[layers[i]]
             plt.figure(num=0, figsize=(12, 6))
@@ -51,6 +54,131 @@ def weights_anal():
             
             plt.savefig(os.path.join(my_path_weights, f'{layers[i]}.png'))
 
+def load_matrix():
+
+    epoch_list = np.linspace(10, 100, 10, dtype='int')
+
+    my_path = './saved/models/matrices'
+    for num in epoch_list:
+        R = np.random.RandomState(seed=num)
+        plt.figure(num=0, figsize=(12, 6))
+        plt.clf()
+        matrix = torch.load(os.path.join(my_path, f'weights_tensor_{num}epoch.pt'))
+        x = np.transpose(matrix.cpu().numpy())
+        # x[x<5] = 0
+        labels = ~np.all(x == 0, axis=1)
+        labels = np.where(labels == True)[0]
+        data = x[~np.all(x == 0, axis=1)]
+        data = np.transpose(data)
+        im = plt.imshow(data, interpolation='none', aspect='auto')
+        plt.colorbar(im)
+        if len(labels) > 1:
+            places = R.choice(len(labels), min(len(labels), 20), replace=False)
+            places.sort()
+            x_labels = np.linspace(0, len(labels), len(labels), dtype='int')
+            x_labels = x_labels[places]
+            y_labels = labels[places]
+            plt.xticks(x_labels, y_labels, rotation=90)
+        plt.savefig(os.path.join(my_path, f'weights_{num}.png'))
+    return None
+
+def svd_tensors():
+
+    epoch_list = np.linspace(10, 100, 10, dtype='int')
+
+    matrices_math = './saved/models/matrices'
+
+    # ==== all eigens ===== #
+
+    total_points = []
+    for i, num in enumerate(epoch_list):
+        matrix = torch.load(os.path.join(matrices_math, f'weights_tensor_{num}epoch.pt'))
+        x = matrix.cpu().numpy()
+        data = x
+        u, s, vh = np.linalg.svd(data, full_matrices=False)
+        print(f'20 first s values for {num} epoch: {s[:20]}\n')
+        X = np.dot(u * s, vh)
+        total_points.append(X)
+    total_points = np.stack(total_points, axis=0)
+    my_path = os.path.join('./', 'saved', 'models', 'matrices', '3d_figures', 'all_eigen')
+    if not os.path.exists(my_path):
+        os.makedirs(my_path)
+    for i, num in enumerate(epoch_list):
+        fig = plt.figure(num=0, figsize=(12, 6))
+        plt.clf()
+        plt.imshow(total_points[i], interpolation='none', aspect='auto')
+        plt.savefig(os.path.join(my_path, f'{num}_epoch'))
+    
+    # ==== 3 eigens ===== #
+
+    total_points = []
+    for i, num in enumerate(epoch_list):
+        matrix = torch.load(os.path.join(matrices_math, f'weights_tensor_{num}epoch.pt'))
+        x = matrix.cpu().numpy()
+        data = x
+        u, s, vh = np.linalg.svd(data, full_matrices=False)
+        s[3:] = 0
+        X = np.dot(u * s, vh)
+        total_points.append(X)
+    total_points = np.stack(total_points, axis=0)
+    my_path = os.path.join('./', 'saved', 'models', 'matrices', '3d_figures', '3_eigen')
+    if not os.path.exists(my_path):
+        os.makedirs(my_path)
+    for i, num in enumerate(epoch_list):
+        fig = plt.figure(num=0, figsize=(12, 6))
+        plt.clf()
+        plt.imshow(total_points[i], interpolation='none', aspect='auto')
+        plt.savefig(os.path.join(my_path, f'{num}_epoch'))
+
+
+    return None
+
+def eigens_scattering():
+    print('here')
+    epoch_list = np.linspace(10, 100, 10, dtype='int')
+
+    matrices_math = './saved/models/matrices'
+
+     # ==== 3 eigens scattering u ===== #
+
+    total_points = []
+    for i, num in enumerate(epoch_list):
+        matrix = torch.load(os.path.join(matrices_math, f'weights_tensor_{num}epoch.pt'), map_location=torch.device('cpu'))
+        x = matrix.cpu().numpy()
+        data = x
+        u, s, vh = np.linalg.svd(data, full_matrices=False)
+        u_sort = u.copy()
+        u_sort.sort(axis=1)
+        u_sort = u_sort[:,::-1]
+        u_scatter = u_sort[:, :3]
+        total_points.append(u_scatter)
+    total_points = np.stack(total_points, axis=0)
+    my_path = os.path.join('./', 'saved', 'models', 'matrices', '3d_figures', 'scattering')
+    if not os.path.exists(my_path):
+        os.makedirs(my_path)
+    for i, num in enumerate(epoch_list):
+        fig = plt.figure(num=0, figsize=(12, 6))
+
+        plt.clf()
+        ax = fig.add_subplot(projection='3d')
+        ax.scatter(total_points[i,:,0], total_points[i,:,1], total_points[i,:,2], marker='o')
+        plt.savefig(os.path.join(my_path, f'{num}_epoch_3d'))
+
+        plt.clf()
+        plt.scatter(total_points[i,:,0], total_points[i,:,1])
+        plt.savefig(os.path.join(my_path, f'{num}_epoch_2d_xy'))
+
+        plt.clf()
+        plt.scatter(total_points[i,:,0], total_points[i,:,2])
+        plt.savefig(os.path.join(my_path, f'{num}_epoch_2d_xz'))
+
+        plt.clf()
+        plt.scatter(total_points[i,:,1], total_points[i,:,2])
+        plt.savefig(os.path.join(my_path, f'{num}_epoch_2d_yz'))
+
 if __name__ == '__main__':
-    means_plotter()
+    # means_plotter()
     # weights_anal()
+    # load_matrix()
+    # svd_tensors()
+    eigens_scattering()
